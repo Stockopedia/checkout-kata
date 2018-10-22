63aeccf463e244a481a91deb9f9673302d080bd4
Euan Goddard <euan@stockopedia.com>
2018-10-22 20:14:24 +0100

    Implemented test outlines

--- test_checkout.py
+++ test_checkout.py
@@ -2,5 +2,50 @@ from nose.tools import eq_
 
 class TestCheckout:
 
-    def test_total(self):
+    def test_simple_total(self):
+        assert False, 'Implement test'
+    
+    def test_empty(self):
+        assert False, 'Implement test'
+    
+    def test_percentage_discount_not_qualifed(self):
+        assert False, 'Implement test'
+
+    def test_percentage_discount_qualified(self):
+        assert False, 'Implement test'
+    
+    def test_percentage_discount_additional(self):
+        assert False, 'Implement test'
+    
+    def test_percentage_discount_multiple_exact(self):
+        assert False, 'Implement test'
+    
+    def test_percentage_discount_multiple_additional(self):
+        assert False, 'Implement test'
+    
+    def test_free_item_below_threshhold(self):
+        assert False, 'Implement test'
+
+    def test_free_item_at_threshold(self):
+        assert False, 'Implement test'
+    
+    def test_free_item_above_threshold(self):
+        assert False, 'Implement test'
+    
+    def test_free_item_exact_multiple(self):
+        assert False, 'Implement test'
+    
+    def test_free_item_multiple_additional(self):
+        assert False, 'Implement test'
+    
+    def test_threshold_save_below_threshold(self):
+        assert False, 'Implement test'
+    
+    def test_threshold_save_at_threshold(self):
+        assert False, 'Implement test'
+
+    def test_threshold_save_above_threshold(self):
+        assert False, 'Implement test'
+
+    def test_other_discount_disqualifies(self):
         assert False, 'Implement test'
