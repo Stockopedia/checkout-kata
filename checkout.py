6a80c3d00072296b7c95ad109758b624c0eb6d0a
Euan Goddard <euan@stockopedia.com>
2018-10-24 15:37:32 +0100

    Implemented functionality to support item discounts

--- checkout.py
+++ checkout.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from pyrecord import Record
 
 class Checkout:
@@ -9,10 +11,24 @@ class Checkout:
 
     def total(self, items):
         total_cost = 0
-        for item_id in items:
-            item = self._stock_by_id[item_id]
-            total_cost += item.cost
+
+        items_counter = Counter(items)
+        for item_id, count in items_counter.items():
+            total_cost += self._total_item(item_id, count)
+        
         return total_cost
+    
+    def _total_item(self, item_id, count):
+        item = self._stock_by_id[item_id]
+        if item.discount:
+            discounts_qualified, items_unqualified_for_discount = divmod(count, item.discount.items)
+            items_qualified_for_discount = item.discount.items * discounts_qualified
+            discounted_item_cost = item.cost * (100 - item.discount.rate) / 100.0
+            total_item_cost = item.cost * items_unqualified_for_discount + discounted_item_cost * items_qualified_for_discount
+        else:
+            total_item_cost = item.cost * count
+        
+        return total_item_cost
 
 
 CheckoutItem = Record.create_type(
